3be1a38e37e0a4e2c57d6d55f8c9f56237d08834
maromo71 <professormoraes@gmail.com>
2019-02-13 09:38:14 -0200

    Aplicativo 1

---
+++ exemplo1/src/br/edu/fatecmm/exemplo1/Main.java
@@ -0,0 +1,9 @@
+package br.edu.fatecmm.exemplo1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("olá Eminfo");
+    }
+}
