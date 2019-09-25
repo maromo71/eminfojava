43d5e9aacfa65a794e0de1d92d92a7ca36e3fd82
maromo71 <professormoraes@gmail.com>
2019-09-25 09:14:08 -0300

    exemplos do dia 25 de setembro

---
+++ ProjetoHeranca/src/net/maromo/view/ControleCaixa.java
@@ -0,0 +1,18 @@
+package net.maromo.view;
+
+import javax.swing.*;
+
+public class ControleCaixa {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField4;
+    private JTextField textField3;
+    private JRadioButton poupançaRadioButton;
+    private JRadioButton correnteRadioButton;
+    private JTextArea textArea1;
+    private JButton depositarButton;
+    private JButton sairButton;
+    private JButton sacarButton;
+    private JButton saldoButton;
+    private JButton registrarContaButton;
+}
